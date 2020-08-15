2b953f28486ba69f58facd47f63c119feded7564
Alex Pinhasov <alexp@selina.com>
2020-08-15 21:14:41 +0300

    Separate files and add new prints

--- Package.swift
+++ Package.swift
@@ -6,14 +6,9 @@ import PackageDescription
 let package = Package(
     name: "AutoLocalized",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "AutoLocalized",
             targets: ["AutoLocalized"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
---
+++ Sources/AutoLocalized/LocalizationCodeFile.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Alex Pinhasov on 15/08/2020.
+//
+
+import Foundation
+
+struct LocalizationCodeFile {
+    let path: String
+    let keys: Set<String>
+}
