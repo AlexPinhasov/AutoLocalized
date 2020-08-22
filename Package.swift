560c9b2aff62ce29e84825803f65940c31cd2dc5
AlexPinhasov <alex5872205@gmail.com>
2020-08-22 13:05:01 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,10 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "AutoLocalized",
-    products: [
-        .executable(name: "autolocalized", targets: ["AutoLocalized"]),
-        .library(name: "AutoLocalized", targets: ["AutoLocalized"])
-    ],
     targets: [
         .target(name: "AutoLocalized", dependencies: []),
         .testTarget(name: "AutoLocalizedTests", dependencies: ["AutoLocalized"]),
